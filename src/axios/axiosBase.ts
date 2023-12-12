f77fd0f7b5b217744a18b75204ff5946bfdf1d19
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-12-12 14:23:59 +0530

    minor changes.

--- src/axios/axiosBase.ts
+++ src/axios/axiosBase.ts
@@ -2,12 +2,12 @@ import axios, { AxiosInstance } from "axios";
 // import "dotenv/config";
 const axiosService: AxiosInstance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
-	withCredentials: true,
+	// withCredentials: true,
 });
 
 export const axiosPrivateService: AxiosInstance = axios.create({
 	baseURL: process.env.REACT_APP_API_URL,
-	withCredentials: true,
+	// withCredentials: true,
 });
 
 export default axiosService;
